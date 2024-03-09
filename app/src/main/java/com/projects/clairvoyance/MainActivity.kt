7a1c9ac9b634c5ec82841f71edd10cb7155c8722
Immersor-git <62318692+Immersor-git@users.noreply.github.com>
2024-03-08 22:59:26 -0800

    Added buggy themes to settings menu

--- app/src/main/java/com/projects/clairvoyance/MainActivity.kt
+++ app/src/main/java/com/projects/clairvoyance/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.projects.clairvoyance
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
@@ -13,12 +16,16 @@ import androidx.activity.addCallback
 import androidx.appcompat.app.ActionBarDrawerToggle
 import com.projects.clairvoyance.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity() : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var fragmentManager: FragmentManager
     private lateinit var binding: ActivityMainBinding
+    private var currentTheme : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        currentTheme = loadTheme()
+        selectTheme(currentTheme)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -51,14 +58,18 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
+        return fragmentNavigation(item.itemId)
+    }
+
+    fun fragmentNavigation(item : Int): Boolean {
+        when(item){
             R.id.bottom_todo -> openFragment(ToDoFragment())
             R.id.bottom_calendar -> openFragment(CalendarFragment())
             R.id.nav_home -> openFragment(ToDoFragment())
             R.id.nav_account -> openFragment(AccountFragment())
             R.id.nav_archive -> openFragment(ArchiveFragment())
             R.id.nav_help -> openFragment(HelpFragment())
-            R.id.nav_settings-> openFragment(SettingsFragment())
+            R.id.nav_settings-> openFragment(SettingsFragment(this, currentTheme))
 
         }
         binding.drawerLayout.closeDrawer(GravityCompat.START)
@@ -69,5 +80,30 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.fragment_container, fragment)
         fragmentTransaction.commit()
+    }
+
+    fun selectTheme(themeIndex : Int) {
+        val themeOptions = resources.getStringArray(R.array.theme_settings)
+        currentTheme = themeIndex
+        saveTheme(themeIndex)
+
+        when(themeIndex){
+            0 -> setTheme(R.style.Base_Theme_Dark)
+            1 -> setTheme(R.style.Base_Theme_Light)
+        }
+
+        Log.d("Main Activity", "Set Theme: "+themeIndex)
+    }
+
+    fun saveTheme(themeIndex : Int) {
+        val preferences : SharedPreferences = getSharedPreferences("PREFERENCE_AppTheme",MODE_PRIVATE)
+        val editor : SharedPreferences.Editor = preferences.edit()
+        editor.putInt("themeIndex", themeIndex)
+        editor.apply()
+    }
+
+    fun loadTheme() : Int {
+        val preferences : SharedPreferences = getSharedPreferences("PREFERENCE_AppTheme",MODE_PRIVATE)
+        return preferences.getInt("themeIndex", 0)
     }
 }
--- app/src/main/java/com/projects/clairvoyance/SettingsFragment.kt
+++ app/src/main/java/com/projects/clairvoyance/SettingsFragment.kt
@@ -1,17 +1,57 @@
 package com.projects.clairvoyance
 
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 
-class SettingsFragment : Fragment() {
+
+class SettingsFragment(private val mainActivity: MainActivity, private val selectedTheme : Int) : Fragment() {
+    lateinit var spinnerThemeOptions : Spinner
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+        createSettingsSpinner(view)
+        return view
+    }
+
+    private fun createSettingsSpinner(view: View) {
+        spinnerThemeOptions = view.findViewById(R.id.spinnerText)
+        val themeOptions = resources.getStringArray(R.array.theme_settings)
+        var adapter = ArrayAdapter<String>(view.context, android.R.layout.simple_spinner_item, themeOptions)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerThemeOptions.setAdapter(adapter)
+        spinnerThemeOptions.setSelection(selectedTheme)
+        spinnerThemeOptions.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Log.d("Settings Buttons","Pressed" + themeOptions.get(position))
+                val lastTheme : Int = selectedTheme
+                mainActivity.selectTheme(position)
+                if (lastTheme != position) {
+                    mainActivity.fragmentNavigation(R.id.nav_settings)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+        Log.d("Settings Buttons","Spinner Generated")
     }
 }
