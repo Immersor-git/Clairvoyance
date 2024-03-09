7fa42cb5381b9c6949ee5a64ce915274a8852694
angeloryan <angeloryansoriano@gmail.com>
2024-03-09 14:38:41 -0800

    Reimplemented work progress 1 files

---
+++ app/src/main/java/com/clairvoyance/clairvoyance/Task.kt
@@ -0,0 +1,30 @@
+package com.clairvoyance.clairvoyance
+
+import android.content.Context
+import androidx.core.content.ContextCompat
+import com.clairvoyance.clairvoyance.R
+import java.time.LocalDate
+import java.time.LocalTime
+import java.util.UUID
+
+// Creates a task item
+class Task (
+    // Data fields, only required fields are name and description
+    // Description can be left blank
+    var name: String,
+    var desc: String,
+    var startTime: LocalTime?,
+    var endTime: LocalTime?,
+    var dueTime: LocalTime?,
+    var date: LocalDate?,
+    var completedDate: LocalDate?,
+    var id: UUID = UUID.randomUUID()
+) {
+    fun isCompleted() = completedDate != null
+    fun imageResource(): Int = if (isCompleted()) R.drawable.checked else R.drawable.unchecked
+    fun imageColor(context: Context): Int = if(isCompleted()) purple(context) else black(context)
+
+    // Color functions for checkbox
+    private fun purple(context: Context) = ContextCompat.getColor(context, R.color.purple_500)
+    private fun black(context: Context) = ContextCompat.getColor(context, R.color.black)
+}
---
+++ app/src/main/java/com/clairvoyance/clairvoyance/TaskClickListener.kt
@@ -0,0 +1,6 @@
+package com.clairvoyance.clairvoyance
+
+interface TaskClickListener {
+    fun editTask(task: Task)
+    fun completeTask(task: Task)
+}
