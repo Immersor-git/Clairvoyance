a1f16875bb54aad6ae1d97afb02189ee75f72f1b
Immersor-git <62318692+Immersor-git@users.noreply.github.com>
2024-04-03 20:50:03 -0700

    firebase partially implemented

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
+    id("com.google.gms.google-services") //Firebase
 }
 
 android {
@@ -83,4 +84,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     debugImplementation("androidx.compose.ui:ui-tooling:1.6.3")
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0")) //Firebase
 }
